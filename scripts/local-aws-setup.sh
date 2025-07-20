c79a255b7035b63d857388cbd27bd05f23164bb9
ptlvrd <patelvrunda027@gmail.com>
2025-07-20 18:20:33 -0400

    Lab 8: Added Hurl integration tests and cases

---
+++ scripts/local-aws-setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+export AWS_SESSION_TOKEN=test
+export AWS_DEFAULT_REGION=us-east-1
+
+echo 'Waiting for LocalStack S3...'
+until (curl --silent http://localhost:4566/_localstack/health | grep "\"s3\": \"\(running\|available\)\"" > /dev/null); do
+    sleep 5
+done
+echo 'LocalStack S3 Ready'
+
+aws --endpoint-url=http://localhost:4566 s3api create-bucket --bucket fragments
+
+aws --endpoint-url=http://localhost:8000 dynamodb create-table \
+    --table-name fragments \
+    --attribute-definitions AttributeName=ownerId,AttributeType=S AttributeName=id,AttributeType=S \
+    --key-schema AttributeName=ownerId,KeyType=HASH AttributeName=id,KeyType=RANGE \
+    --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=5
+
+aws --endpoint-url=http://localhost:8000 dynamodb wait table-exists --table-name fragments
